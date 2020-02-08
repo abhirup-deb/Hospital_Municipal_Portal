65d805cf6ee7c21c22edac47b6fd9de799756611
abhirup-deb <debabhirup1999@gmail.com>
2020-02-08 22:22:46 +0530

    Screens created and registered

---
+++ Desktop/hospital_portal/lib/screens/home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class home extends StatefulWidget {
+  static const String id = 'home';
+  @override
+  _homeState createState() => _homeState();
+}
+
+class _homeState extends  State<home>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
