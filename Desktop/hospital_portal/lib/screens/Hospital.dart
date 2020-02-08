b5afcec9afc53345718a993b3ce7b752f0f614af
abhirup-deb <debabhirup1999@gmail.com>
2020-02-08 23:18:43 +0530

    Screens' skeleton created

---
+++ Desktop/hospital_portal/lib/screens/Hospital.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Hospital extends StatefulWidget {
+  static const String id = 'hospital';
+  @override
+  _HospitalState createState() => _HospitalState();
+}
+
+class _HospitalState extends  State<Hospital>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ Desktop/hospital_portal/lib/screens/Verification.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Verification extends StatefulWidget {
+  static const String id = 'verification';
+  @override
+  _VerificationState createState() => _VerificationState();
+}
+
+class _VerificationState extends  State<Verification>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
