e804eeb6466e0d111552e1cce24c7c91640919ac
abhirup-deb <debabhirup1999@gmail.com>
2020-02-09 19:10:10 +0530

    List Tiles created and Linked

--- Desktop/hospital_portal/lib/screens/Homepage.dart
+++ Desktop/hospital_portal/lib/screens/Homepage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hospital_portal/screens/Hospital.dart';
+import 'package:hospital_portal/screens/Registration.dart';
+import 'package:hospital_portal/screens/Verification.dart';
 
 class Homepage extends StatefulWidget {
   static const String id = 'Homepage';
@@ -7,6 +10,16 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends  State<Homepage>{
+
+  call_page(int value){
+    if(value==1)
+
+    if(value==2)
+      Navigator.pushNamed(context, Verification.id);
+    if(value==3)
+      Navigator.pushNamed(context, Hospital.id);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,7 +27,7 @@ class _HomepageState extends  State<Homepage>{
       ),
       drawer: Drawer(child: ListView(
         padding: EdgeInsets.zero,
-        children: const <Widget>[
+        children:  <Widget>[
           DrawerHeader(
             decoration: BoxDecoration(
               color: Color(0xFF01579B),
@@ -30,14 +43,23 @@ class _HomepageState extends  State<Homepage>{
           ListTile(
             leading: Icon(Icons.border_color),
             title: Text('Register'),
+            onTap: () {
+              Navigator.pushNamed(context, Registration.id);
+            },
           ),
           ListTile(
             leading: Icon(Icons.verified_user),
             title: Text('Verification'),
+            onTap: () {
+              Navigator.pushNamed(context, Verification.id);
+            },
           ),
           ListTile(
             leading: Icon(Icons.local_hospital),
             title: Text('Hospital'),
+            onTap: () {
+              Navigator.pushNamed(context, Hospital.id);
+            },
           ),
         ],
       ),)
