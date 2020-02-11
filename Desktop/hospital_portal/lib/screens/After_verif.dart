893b747f1d2eb9f9975724ac3c753b4544836d09
abhirup-deb <debabhirup1999@gmail.com>
2020-02-11 21:13:36 +0530

    After_verif added

---
+++ Desktop/hospital_portal/lib/screens/After_verif.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class After_verif extends StatefulWidget{
+  static const String id = 'After_verif';
+  _After_verifState createState() => _After_verifState();
+}
+
+class _After_verifState extends State<After_verif>{
+  Widget build (BuildContext context) {
+    return Scaffold();
+  }
+}
