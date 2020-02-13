4077beccc2dd6df5ece28ae2d87fdaa8466ac9f4
abhirup-deb <debabhirup1999@gmail.com>
2020-02-13 21:13:05 +0530

    SearchBar Added

--- Desktop/hospital_portal/lib/screens/Homepage.dart
+++ Desktop/hospital_portal/lib/screens/Homepage.dart
@@ -55,6 +55,7 @@ class _HomepageState extends  State<Homepage>{
           ),
         ],
       ),)
+
     );
   }
 }
--- Desktop/hospital_portal/lib/screens/Hospital.dart
+++ Desktop/hospital_portal/lib/screens/Hospital.dart
@@ -3,6 +3,8 @@ import 'package:hospital_portal/screens/Homepage.dart';
 import 'package:hospital_portal/screens/Verification.dart';
 import 'package:hospital_portal/screens/Registration.dart';
 
+import 'package:flappy_search_bar/flappy_search_bar.dart';
+
 class Hospital extends StatefulWidget {
   static const String id = 'hospital';
   @override
@@ -10,9 +12,13 @@ class Hospital extends StatefulWidget {
 }
 
 class _HospitalState extends  State<Hospital>{
+  SearchBarController _searchBarController;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Hospital'),
+    return Scaffold(appBar: AppBar(title: Text('Hospital'),actions: <Widget>[
+
+    ],
     ),
         drawer: Drawer(child: ListView(
           padding: EdgeInsets.zero,
@@ -52,8 +58,64 @@ class _HospitalState extends  State<Hospital>{
             ),
 
           ],
-        ),)
-
+        ),),
+      body: SafeArea(
+        child: SearchBar<Post>(
+          searchBarPadding: EdgeInsets.symmetric(horizontal: 10),
+          headerPadding: EdgeInsets.symmetric(horizontal: 10),
+          listPadding: EdgeInsets.symmetric(horizontal: 10),
+          onSearch: _getALlPosts,
+          searchBarController: _searchBarController,
+          placeHolder: Text("placeholder"),
+          cancellationWidget: Text("Cancel"),
+          emptyWidget: Text("empty"),
+          indexedScaledTileBuilder: (int index) => ScaledTile.count(1, index.isEven ? 2 : 1),
+          header: Row(
+            children: <Widget>[
+              RaisedButton(
+                child: Text("sort"),
+                onPressed: () {
+                  _searchBarController.sortList((Post a, Post b) {
+                    return a.body.compareTo(b.body);
+                  });
+                },
+              ),
+              RaisedButton(
+                child: Text("Desort"),
+                onPressed: () {
+                  _searchBarController.removeSort();
+                },
+              ),
+              RaisedButton(
+                child: Text("Replay"),
+                onPressed: () {
+                  isReplay = !isReplay;
+                  _searchBarController.replayLastSearch();
+                },
+              ),
+            ],
+          ),
+          onCancelled: () {
+            print("Cancelled triggered");
+          },
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+          crossAxisCount: 2,
+          onItemFound: (Post post, int index) {
+            return Container(
+              color: Colors.lightBlue,
+              child: ListTile(
+                title: Text(post.title),
+                isThreeLine: true,
+                subtitle: Text(post.body),
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => Detail()));
+                },
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
