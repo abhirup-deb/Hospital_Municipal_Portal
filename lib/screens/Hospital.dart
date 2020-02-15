8395cd82aeb3236724428e4eb43c675eb224f8b5
abhirup-deb <debabhirup1999@gmail.com>
2020-02-15 16:25:03 +0530

    Hospital list Added

--- lib/screens/Hospital.dart
+++ lib/screens/Hospital.dart
@@ -15,6 +15,8 @@ class Post {
   Post(this.title, this.description);
 }
 
+Container
+
 class Hospital extends StatefulWidget {
   static const String id = 'hospital';
   @override
@@ -81,44 +83,106 @@ class _HospitalState extends  State<Hospital>{
 
           ],
         ),),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15),
-          child: SearchBar<Post>(
-            textStyle: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 200.0,
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: SearchBar<Post>(
+                  textStyle: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                  hintText: "Enter your Name here..",
+                  hintStyle: TextStyle(
+                    color: Colors.grey[100],
+                  ),
+                  debounceDuration: Duration(milliseconds: 800),
+                  minimumChars: 5,
+                  crossAxisCount: 2,
+                  indexedScaledTileBuilder: (int index) => ScaledTile.count(
+                    index % 3 == 0 ? 2 : 1,
+                    1,
+                  ),
+                  cancellationWidget: Text("ok"),
+                  loader: Center(child: Text('Loading.....')),
+                  searchBarStyle: SearchBarStyle(
+                    backgroundColor: Colors.white70,
+                    padding: EdgeInsets.all(5),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  onSearch: search,
+                  onItemFound: (Post post, int index) {
+                    return ListTile(
+                      title: Text(post.title),
+                      subtitle: Text(post.description),
+                    );
+                  },
+                ),
+              ),
             ),
-            mainAxisSpacing: 10,
-            crossAxisSpacing: 10,
-            hintText: "Enter your Name here..",
-            hintStyle: TextStyle(
-              color: Colors.grey[100],
-            ),
-            debounceDuration: Duration(milliseconds: 800),
-            minimumChars: 5,
-            crossAxisCount: 2,
-            indexedScaledTileBuilder: (int index) => ScaledTile.count(
-              index % 3 == 0 ? 2 : 1,
-              1,
-            ),
-            cancellationWidget: Text("ok"),
-            loader: Center(child: Text('Loading.....')),
-            searchBarStyle: SearchBarStyle(
-              backgroundColor: Colors.white70,
-              padding: EdgeInsets.all(5),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            onSearch: search,
-            onItemFound: (Post post, int index) {
-              return ListTile(
-                title: Text(post.title),
-                subtitle: Text(post.description),
-              );
-            },
           ),
-        ),
+          SizedBox( height: 10.0,),
+          Text('List of Hospitals Available',style: TextStyle(fontSize: 28.0,fontWeight: FontWeight.bold,fontStyle: FontStyle.italic),textAlign: TextAlign.center,),
+          SizedBox( height: 10.0,),
+          Container(margin: EdgeInsets.all(15.0),
+            height: 200.0,
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.greenAccent),
+              borderRadius: BorderRadius.all(Radius.circular(14.0)),
+            ),
+            child: ListView(children: <Widget>[
+
+              Container(margin: EdgeInsets.all(10.0),
+                decoration: BoxDecoration(border: Border(
+                  bottom: BorderSide( //                   <--- left side
+                    color: Colors.blueGrey,
+                    width: 3.0,
+                  ),),),
+                child: Text("💉     Peerless Hospital ",style: TextStyle(fontSize: 18.0),),),
+
+              Container(margin: EdgeInsets.all(10.0),
+                decoration: BoxDecoration(border: Border(
+                  bottom: BorderSide( //                   <--- left side
+                    color: Colors.blueGrey,
+                    width: 3.0,
+                  ),),),
+                child: Text("💉     Fortis Hospital ",style: TextStyle(fontSize: 18.0),),),
+
+              Container(margin: EdgeInsets.all(10.0),
+                decoration: BoxDecoration(border: Border(
+                  bottom: BorderSide( //                   <--- left side
+                    color: Colors.blueGrey,
+                    width: 3.0,
+                  ),),),
+                child: Text("💉     Woodlands ",style: TextStyle(fontSize: 18.0),),),
+
+              Container(margin: EdgeInsets.all(10.0),
+                decoration: BoxDecoration(border: Border(
+                  bottom: BorderSide( //                   <--- left side
+                    color: Colors.blueGrey,
+                    width: 3.0,
+                  ),),),
+                child: Text("💉     Ruby General Hospital ",style: TextStyle(fontSize: 18.0),),),
+
+              Container(margin: EdgeInsets.all(10.0),
+                decoration: BoxDecoration(border: Border(
+                  bottom: BorderSide( //                   <--- left side
+                    color: Colors.blueGrey,
+                    width: 3.0,
+                  ),),),
+                child: Text("💉     Command Hospital ",style: TextStyle(fontSize: 18.0),),),
+          ],
+          ),
+            
+          ),
+        ],
       ),
+
     );
   }
 }
